5d9eca70ccae85da52dcf84adfb2d1df5265671a
Fahad Assoumani <fahad.assoumani@epitech.eu>
2022-12-16 16:11:47 +0100

    init: main with file reader

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func getFileContent(file string) (*os.File, *bufio.Scanner) {
+	fd, err := os.Open(file)
+	if err != nil {
+		println(err)
+		panic("cannot open file")
+	}
+	fmt.Println(file)
+	fileScanner := bufio.NewScanner(fd)
+	fileScanner.Split(bufio.ScanLines)
+	return fd, fileScanner
+}
+
+func main() {
+	fmt.Println(os.Args)
+
+	if len(os.Args) != 2 {
+		panic("missing input file")
+	}
+	file := os.Args[1]
+	fd, f := getFileContent(file)
+	defer fd.Close()
+	for f.Scan() {
+		fmt.Println(f.Text())
+	}
+}
